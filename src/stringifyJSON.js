33e3368075e51895f1cdb5abb967947edfac8bfe
Felicia Fong <feliciasfong@gmail.com>
2016-08-28 17:28:43 -0700

    Solution to stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,33 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  if  (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  
+  if (Array.isArray(obj)) {
+  	return "[" + obj.map(function(el) { return stringifyJSON(el); }).join() + "]";
+  }
+
+  if (typeof obj === 'object') {
+ 	var result = [];
+ 	for (var key in obj) {
+ 		if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+ 			result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+ 		}	
+ 	}
+    return "{" + result.join() + "}";
+  }
+
 };
+
+
